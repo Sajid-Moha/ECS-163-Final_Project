dfcf29937b7898c6ec45d84d68aec4124c9f0175
XiangM7 <157428241+XiangM7@users.noreply.github.com>
2025-05-27 18:15:43 -0700

    Update shrink.py

--- datasets/shrink.py
+++ datasets/shrink.py
@@ -7,4 +7,4 @@ with pd.read_csv('dataset_minimization\TMDB_movie_dataset_v11.csv', usecols=colu
     for i, chunk in enumerate(reader):
         mode = 'w' if i == 0 else 'a'
         header = i == 0
-        chunk.to_csv('dataset_minimization\TMDB_movie_dataset_reduced.csv', mode=mode, header=header, index=False)
+        chunk.to_csv('dataset_minimization\movie.csv', mode=mode, header=header, index=False)
